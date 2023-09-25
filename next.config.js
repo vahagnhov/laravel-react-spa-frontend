f4a8b8419b1a86dfdfe251dd200ee923e079b405
vahagnhov <vahagn505315v@gmail.com>
2023-09-25 12:06:02 +0400

    Stripe in frontend

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        apiUrl: process.env.API_URL | '127.0.0.1:8000/api',
+        stripeKey: process.env.STRIPE_KEY,
+    },
+}
 
 module.exports = nextConfig
